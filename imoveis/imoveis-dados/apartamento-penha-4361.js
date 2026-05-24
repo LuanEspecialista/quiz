e05172ff88182a5b114eddbffc71f4fb75350873
LuanEspecialista <luan.investimento.certo@gmail.com>
2026-05-24 18:52:44 -0300

    Add files via upload

---
+++ imoveis/imoveis-dados/apartamento-penha-4361.js
@@ -0,0 +1,41 @@
+window.IMOVEIS.push({
+
+  codigo: "4361",
+
+  slug: "apartamento-penha-4361",
+
+  titulo: "Apartamento no Baía Azul Beach Club",
+
+  tipo: "Apartamento",
+
+  status: "Disponível",
+
+  valor: "R$ 539.000,00",
+
+  cidade: "Penha - SC",
+
+  endereco: "Avenida Vereador Manoel Henrique de Assis - Centro - Penha - SC",
+
+  areaUtil: "52,48m²",
+
+  areaTotal: "64,77m²",
+
+  dormitorios: "2",
+
+  banheiros: "1",
+
+  vagas: "1",
+
+  entrega: "Agosto de 2027",
+
+  pasta: "apartamento-penha-4361",
+
+  totalImagens: 36,
+
+  descricaoCurta:
+    "Apartamento em construção com lazer completo e excelente potencial de valorização.",
+
+  descricao:
+    "More ou invista em um condomínio completo pensado para oferecer lazer, segurança e qualidade de vida a poucos minutos do mar."
+
+});
